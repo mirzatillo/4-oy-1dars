bdb2af22b5282dec107a81c0e095542b64930f68
mirzatillo <ibragimovmirzatillo27@gamil.com>
2024-07-20 16:03:29 +0500

    vazifa

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
-let A = +prompt('raqam kiriting');
-let natija = Math.floor(A / 2);
-let qoldiq = A % 2;
-console.log(natija);
-console.log(qoldiq);
+// let A = +prompt('raqam kiriting');
+// let natija = Math.floor(A / 2);
+// let qoldiq = A % 2;
+// console.log(natija);
+// console.log(qoldiq);
 
-let A = +prompt('raqam kriting');
-let B = +prompt('raqam kriting');
-if (A > B) {
-    let kopaytma = A * B;
-    console.log(kopaytma);
-}else if (A == B) {
-let ayirma = A - B;
-console.log(ayirma);
-}else {
-    let yigindi = A + B;
-console.log(yigindi);
-}
+// let A = +prompt('raqam kriting');
+// let B = +prompt('raqam kriting');
+// if (A > B) {
+//     let kopaytma = A * B;
+//     console.log(kopaytma);
+// }else if (A == B) {
+// let ayirma = A - B;
+// console.log(ayirma);
+// }else {
+//     let yigindi = A + B;
+// console.log(yigindi);
+// }
 
-let A = +prompt('son kiritinng');
-let result = 1;
-for (let i = 2; i <= A ; i++ ){
-result = result*i 
-}
-console.log(result);
+// let A = +prompt('son kiritinng');
+// let result = 1;
+// for (let i = 2; i <= A ; i++ ){
+// result = result*i 
+// }
+// console.log(result);
 
-let A = +prompt('son kirit');
-let B = +prompt('son kirit');
-let C = +prompt('son kirit');
-let resa = A * A ;
-let resb = B * B ;
-let resc = C * C ;
-let result = resa + resb + resc;
-console.log(result);
+// let A = +prompt('son kirit');
+// let B = +prompt('son kirit');
+// let C = +prompt('son kirit');
+// let resa = A * A ;
+// let resb = B * B ;
+// let resc = C * C ;
+// let result = resa + resb + resc;
+// console.log(result);
 
-5. Berilgan ikki sonning darajasini hisoblang (A^B).
-let A = +prompt('son kiriting');
-let B = +prompt('son kiriting');
-let result = A**B;
-console.log(result);
+// 5. Berilgan ikki sonning darajasini hisoblang (A^B).
+// let A = +prompt('son kiriting');
+// let B = +prompt('son kiriting');
+// let result = A**B;
+// console.log(result);
 
+
+// 6-masala
+// let a = +prompt('son kiriting');
+// let ildiz = Math.sqrt(a);
+// let yaxlid = Math.round(ildiz);
+// console.log(ildiz);
+// console.log(yaxlid);
+
+//  7 masala
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// res = 1;
+// result = 1;
+// for(let i = 1; i <= 5 ; i++){
+// res = res * a;
+// }
+// console.log(res);
+// for(let n = 1; n <= 5 ; n++){
+// result = result * b;
+// }
+// console.log(result);
+
+//   8 masala
+// let a = +prompt('son kiriting');
+// let result = a * 10 - 7;
+// console.log(result);
+
+//   9 masala
+// let a = +prompt('son kirit');
+// let result = a*a*a;
+// console.log(result);
+
+//   10 masala
+// let a = +prompt('son kiritin');
+// let b = +prompt('son kiritin');
+// let c = +prompt('son kiritin');
+// let res = Math.pow(a*b*c,1/3);
+// let butun = Math.round(res)
+// console.log(butun);
